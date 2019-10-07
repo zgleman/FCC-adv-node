c5bf6ee2056bb5b34d38af83358d4836415e2358
Glitch (mercurial-platinum) <none>
2019-10-07 12:59:24 +0000

    🍚🚈 Checkpoint ./server.js:7037687/2266

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const app = express();
+const mongo = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectId;
 
 process.env.SESSION_SECRET = 23.4;
+process.env.DATABASE = 'mongodb+srv://zgleman:grey1127@cluster0-2my3z.mongodb.net/test?retryWrites=true&w=majority';
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -18,15 +20,7 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser((user, done)=>{
-  done(null, user._id);
-})
-passport.deserializeUser((id, done)=>{
-  /*db.collection('users').findOne(
-  {_id: new ObjectID(id)},
-  (err, doc)=> done(null, doc);*/
-  done(null, null);
-})
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
@@ -37,6 +31,27 @@ app.route('/')
     res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please login'});
   });
 
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if(err) {
+        console.log('Database error: ' + err);
+    } else {
+        console.log('Successful database connection');
+      
+passport.serializeUser((user, done)=>{
+  done(null, user._id);
+});
+passport.deserializeUser((id, done)=>{
+  db.collection('users').findOne(
+  {_id: new ObjectID(id)},
+  (err, doc)=> {
+    done(null, doc);
+  })
+  
+});
+
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 });
+    }});
