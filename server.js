27d61eff8883fd12d04e36b2ff45b65b5fe0e176
Glitch (mercurial-platinum) <none>
2019-10-07 12:36:19 +0000

    🚀🍬 Checkpoint ./server.js:7037687/352 ./package.json:7037687/70

--- server.js
+++ server.js
@@ -3,17 +3,27 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
 
+process.env.SESSION_SECRET = 23.4;
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please login'});
   });
 
 app.listen(process.env.PORT || 3000, () => {
