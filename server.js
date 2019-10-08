e99bd1243fa4a3c0412a34df5dfc895ebdce41ca
Glitch (mercurial-platinum) <none>
2019-10-08 08:38:35 +0000

    👞🍿 Checkpoint ./server.js:7037687/684

--- server.js
+++ server.js
@@ -77,9 +77,27 @@ app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
-app.post('/register', function(req, res){
-  
-})
+app.post('/register', function(req, res, next){
+  db.collection('users').findOne({username: req.body.username}), function (err, user){
+    if (err) {
+      next(err);
+    } else if (user) {
+      res.redirect('/');
+    }
+    else {
+      db.collection('users').insertOne({
+        username: req.body.username,
+        password: req.body.password},
+        (err, doc) => {
+        if(err) {
+          res.redirect('/');
+        } else {
+          next(null, user);
+        }
+      });
+      }
+    }  
+}, passport.authenticate('local', {failureRedirect: '/'}, function(req,)));
 app.use((req,res, next)=>{
   res.status(404)
   .type('text')
