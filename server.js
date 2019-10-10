30074c05cea7dd06e02a83043948c90490262c4e
Glitch (mercurial-platinum) <none>
2019-10-10 14:15:10 +0000

    📖🕷 Checkpoint ./server.js:7037687/375 ./package.json:7037687/24

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const session = require("express-session");
 const passport = require("passport");
 const app = express();
 const mongo = require("mongodb").MongoClient;
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+
+
 const routes = require('./Routes.js');
 const auth = require('./Auth.js');
 
@@ -41,12 +41,6 @@ mongo.connect(process.env.DATABASE, (err, db) => {
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + process.env.PORT);
     });
-    var currentUsers = 0;
-    io.on('connection', socket => {
-  console.log('A user has connected');
-    ++currentUsers;
-      io.emit('user count', currentUsers);
-});
     
 
 
